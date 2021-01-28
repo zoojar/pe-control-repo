529374b8dfd7632d630471187a9f53f173ce021e
David Newton <davidnewton82@gmail.com>
2021-01-28 12:54:12 +0000

    Update base.pp

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,7 +1,11 @@
 class profile::base (
   String $ntp_server
+  String $motd_content
 ){
 
   #the base profile should include component modules that will be on all nodes
+  class { 'motd':
+    content => $motd_content,
+  }
 
 }
