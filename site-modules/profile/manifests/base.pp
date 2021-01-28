66f4a15dbfd389357b2c2686c3e4830e971e1084
davidn <david@davidjnewton.com>
2021-01-28 15:18:10 +0000

    data & ntp profile

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,8 +1,6 @@
-class profile::base (
-  String $ntp_server
-){
-
+class profile::base {
   #the base profile should include component modules that will be on all nodes
   include profile::motd
+  include profile::ntp
 
 }
---
+++ site-modules/profile/manifests/ntp.pp
@@ -0,0 +1,13 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile::ntp
+class profile::ntp (
+  Array[String] $servers
+){
+  class { 'ntp':
+    servers => $servers,
+  }
+}
