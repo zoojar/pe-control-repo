ae6f6c33a1ca857ec1dd89cb3cc8fbbd8badb55e
David Newton <davidnewton82@gmail.com>
2021-01-28 15:24:37 +0000

    Update motd.pp

--- site-modules/profile/manifests/motd.pp
+++ site-modules/profile/manifests/motd.pp
@@ -1,10 +1,7 @@
 class profile::motd (
   String $content
 ){
-
-  #the base profile should include component modules that will be on all nodes
   class { 'motd':
     content => $content,
   }
-
 }
