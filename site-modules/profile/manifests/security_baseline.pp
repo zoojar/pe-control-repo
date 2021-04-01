e4e03ee9784b138cd9096e2c244159261dd6f8cf
David Newton <davidnewton82@gmail.com>
2021-04-01 17:48:25 +0100

    Update security_baseline.pp

--- site-modules/profile/manifests/security_baseline.pp
+++ site-modules/profile/manifests/security_baseline.pp
@@ -1,6 +1,12 @@
 # security baseline profile
 class profile::security_baseline (
-
+  Array[String] $time_servers = ['tick.usno.navy.mil', 'tock.usno.navy.mil'],
+  String        $profile_type = 'server' ,
+  Array[String] $remote_access_allowed_users = ['vagrant'],
 ) {
-  
+  class { '::secure_linux_cis':
+    time_servers  => $time_servers,
+    profile_type  => $profile_type,
+    allow_users   => $remote_access_allowed_users,
+  } 
 }
