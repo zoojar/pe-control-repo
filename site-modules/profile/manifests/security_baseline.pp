4114305a34c6ac0affabd184b25b254f932c9e72
David Newton <davidnewton82@gmail.com>
2021-04-01 18:00:09 +0100

    Update security_baseline.pp

--- site-modules/profile/manifests/security_baseline.pp
+++ site-modules/profile/manifests/security_baseline.pp
@@ -3,10 +3,12 @@ class profile::security_baseline (
   Array[String] $time_servers = ['0.uk.pool.ntp.org', '1.uk.pool.ntp.org'],
   String        $profile_type = 'server' ,
   Array[String] $remote_access_allowed_users = ['vagrant'],
+  Array[String] $host_allow_rules, 
 ) {
   class { '::secure_linux_cis':
-    time_servers  => $time_servers,
-    profile_type  => $profile_type,
-    allow_users   => $remote_access_allowed_users,
+    time_servers     => $time_servers,
+    profile_type     => $profile_type,
+    allow_users      => $remote_access_allowed_users,
+    host_allow_rules => $host_allow_rules,
   } 
 }
