62e6a9670012619ecaa8297f3551ad5149aa326d
djn <some@email.com>
2024-09-17 16:11:50 +0100

    classify

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,7 +25,7 @@ File { backup => false }
 #
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
+  if $trusted['extensions']['pp_role'] {
+    include "role::${trusted['extensions']['pp_role']}"
+  }
 }
