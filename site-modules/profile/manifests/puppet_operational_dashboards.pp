cadcfe06ab16fd4e0a1a7fea1afb1db3e6f32c5c
djn <some@email.com>
2024-09-19 11:42:26 +0100

    dashboard role and profile

---
+++ site-modules/profile/manifests/puppet_operational_dashboards.pp
@@ -0,0 +1,9 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile::puppet_operational_dashboards
+class profile::puppet_operational_dashboards {
+  include puppet_operational_dashboards
+}
---
+++ site-modules/role/manifests/dashboard.pp
@@ -0,0 +1,9 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include role::dashboard
+class role::dashboard {
+  include profile::puppet_operational_dashboards
+}
