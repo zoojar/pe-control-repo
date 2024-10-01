0cc3ef636f5d59ca9c9f2154bc6162ab98cd95f1
David Newton <david@davidjnewton.com>
2024-10-01 15:31:39 +0100

    yaml format

--- site-modules/profile/manifests/puppet_operational_dashboards.pp
+++ site-modules/profile/manifests/puppet_operational_dashboards.pp
@@ -4,19 +4,41 @@
 #
 # @example
 #   include profile::puppet_operational_dashboards
-class profile::puppet_operational_dashboards {
-  include puppet_operational_dashboards
-  #class { 'puppet_operational_dashboards::profile::dashboards':
-  #  grafana_port => 443,
-  #}
-  file { '/etc/grafana/conf.d/auth_viewer.ini':
-    ensure  => file,
-    content => @(EOF)
-      [auth.anonymous]
-      enabled = true
-      org_role = Viewer
-      | EOF
-    ,
+# @param grafana_config_file The path to the Grafana configuration file
+class profile::puppet_operational_dashboards (
+  String $grafana_config_file = '/etc/grafana/grafana.ini',
+) {
+  class { 'puppet_operational_dashboards':
+    template_format => 'yaml',
+  }
+  file_capability { '/usr/sbin/grafana-server':
+    capability => 'cap_net_bind_service=+ep',
+  }
+  Ini_setting {
     notify  => Service['grafana-server'],
+  }
+  ini_setting { 'auth.anonymous-enabled':
+    path    => $grafana_config_file,
+    section => 'auth.anonymous',
+    setting => 'enabled',
+    value   => true,
+  }
+  ini_setting { 'auth.anonymous-org_role':
+    path    => $grafana_config_file,
+    section => 'auth.anonymous',
+    setting => 'org_role',
+    value   => 'Viewer',
+  }
+  ini_setting { 'auth.basic-enabled':
+    path    => $grafana_config_file,
+    section => 'auth.basic',
+    setting => 'enabled',
+    value   => false,
+  }
+  ini_setting { 'auth-disable_login_form':
+    path    => $grafana_config_file,
+    section => 'auth',
+    setting => 'disable_login_form',
+    value   => true,
   }
 }
