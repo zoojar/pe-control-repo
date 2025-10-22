00aa705b6412bc0fd3c8a5fc885c6f0798568808
davidjn <david.newton@perforce.com>
2025-10-22 14:10:37 +0100

    example role

--- site-modules/role/manifests/example.pp
+++ site-modules/role/manifests/example.pp
@@ -1,4 +1,6 @@
 # An example role
 class role::example {
-
+  include profile::example
+  $testdata = lookup('puppet_operational_dashboards::profile::dashboards::grafana_port', { 'default_value' => 'default value' })
+  notify { "grafana_port is ${testdata}": }
 }
